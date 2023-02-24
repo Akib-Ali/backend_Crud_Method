2e42977bb2c63764f37f71e5ffa60be435f4f655
Akib Ali <akib7599@gmail.com>
2023-02-24 15:33:16 +0530

    add user schema plus notes schema plus mongoose connect

--- cheezy_code_api/src/index.js
+++ cheezy_code_api/src/index.js
@@ -1,1 +1,27 @@
-console.log("welcome to ist node api")
+const express = require("express");
+const noteRouter = require("./routes/noteRoutes");
+const userRouter = require("./routes/userRoutes");
+const app = express();
+const mongoose = require("mongoose")
+
+app.use("/users", userRouter);
+app.use('/note', noteRouter);
+
+app.get("/", (req,res)=>{
+    res.status(200).send("Hello My Ist api")
+
+});
+
+mongoose.connect("mongodb://localhost:27017/e-com")
+.then(()=>{
+
+    app.listen(5000,()=>{
+        console.log("Server started on port number 5000")
+    })
+    
+})
+.catch((error)=>{
+    console.log(error)
+
+})
+
