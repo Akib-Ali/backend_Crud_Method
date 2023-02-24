6df586d2a1466fea65892e5e392ca14eed0921c2
Akib Ali <akib7599@gmail.com>
2023-02-24 12:43:58 +0530

    api with middleware cheezy_code

--- final_api/index.js
+++ final_api/index.js
@@ -6,6 +6,30 @@ let Product = require('./product');
 const app = express();
 app.use(express.json())
 
+
+//middleware
+
+const reqfilter=(req,resp,next)=>{
+
+    if(!req.query.name){
+        resp.send('Please provide author name')
+
+    }else if(req.query.name != "akib"){
+        resp.send('You Are not right person')
+        
+    }else{
+        next()
+    }
+
+
+
+}
+
+app.use(reqfilter)
+
+
+//post api
+
 app.post("/create", async (req,resp)=>{
    let data = new Product(req.body)
    const  result = await data.save()
